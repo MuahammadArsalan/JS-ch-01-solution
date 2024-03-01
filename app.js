150150e6bfc64d8de1835d7503adc30472544c49
MuhammadArsalan <muhammadarsalanforever@gmail.com>
2024-03-01 13:33:13 +0530

    update

---
+++ app.js
@@ -0,0 +1,67 @@
+// Question no-01:
+//  Write a script to greet your website visitor using JS alert  box.
+
+// solution:
+
+// alert("Hello visitor thnaks for visiting our website");
+
+// Question no-02:
+// Write a script to display following message on your web page.
+
+//  Solution:
+// alert("Error! Please enter a valid password");
+// var p = prompt("Error! Please enter a valid password");
+
+
+// Question no-03:
+//Write a script to display following message on your web page "Welcome to js land (br) Happy coding".
+
+// Solution:
+
+// alert("Welcome to JS land... \n Happy coding !");
+
+// Question no-04:
+// Write a script to display following messages in sequence:
+
+// Solution:
+
+// alert("Welcome to JS land ........");
+
+// alert("Happy Coding ! .................");
+
+
+// Question no-05:           (incomplete)
+//Generate the following message through browser’s developer console.
+
+// Solution:
+
+//  var a = alert("Hello...I can run JS through my web browsers's console.");
+
+// console.log (a);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
